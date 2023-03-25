a1738195e73e764fb551acb0edbd3a592e8f38c6
Matheus-Mazieiro <matheus.mazieiro@catijr.com.br>
2023-03-25 13:57:46 -0300

    Mail Confirmation screen

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home/index';
+import MailConfirmation from '../pages/Desktop-4'
 
 const AppRoutes = () => (
   <Routes>
     {/* exemplo de rota: <Route path="/novaRota" element={<ArquivoImportado />} /> */}
+    <Route path="/MailConfirmation" element={<MailConfirmation />} />
     <Route path="/" element={<Home />} />
   </Routes>
 );
