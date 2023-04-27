23565b6bbaebf5de712d0fa9b7106cdca797e185
Danilo da Silva Pinto <danilo.silva@catijr.com.br>
2023-04-26 23:58:49 -0300

    Feat: finalizada página de condições da loja, porém ainda sem responsividade mobile

--- src/pages/ShopConditions/style.tsx
+++ src/pages/ShopConditions/style.tsx
@@ -18,3 +18,19 @@ export const FLEXCOLUMN = styled.div`
   justify-content: center;
 `;
 
+export const TITLE = styled.text`
+    margin-top: rh(57);
+    font-size: ${rf(48)};
+    line-height: 58px;
+    font-weight: 700;
+    color: ${THEME.COLORS.PRIMARY};
+    text-align: center;
+    font-family: 'Inter';
+`;
+
+export const TEXT = styled.p`
+    font-family: 'Inter';
+    font-size: ${rf(24)};
+    font-weight: 400;
+    line-height: 29px;
+`;
