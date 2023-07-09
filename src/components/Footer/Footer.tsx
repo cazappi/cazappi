d90e6e5070bd8ff967cdc543a1f6804ed7922517
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2023-07-09 14:35:08 -0300

    Feat: corrigindo responsividade, finalizando a primeira página e começando a inserir animações onScroll

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,9 +10,9 @@ import { Icon } from '@iconify-icon/react';
 const Footer: React.FC = () => {
   return (
     <div className="bg-GRAY_600 text-WHITE flex flex-col items-center justify-center py-10">
-      <div className="flex flex-row justify-between text-sm w-2/3">
+      <div className="flex flex-row justify-between text-xs md:text-sm w-11/12 md:w-2/3">
         <div className="flex flex-col mb-10">
-          <img src={logoBranca} alt="" className="" />
+          <img src={logoBranca} alt="" className="w-36 md:w-auto" />
           <a href="">Fale com a gente</a>
           <a href="">Termos de uso</a>
           <a href="">Privacidade</a>
@@ -20,9 +20,9 @@ const Footer: React.FC = () => {
         </div>
         <div className="flex flex-col">
           <div className="text-lg font-bold">Conheça mais</div>
-          <a href="">Termos de uso</a>
-          <a href="">Privacidade</a>
-          <a href="">Aplicativo</a>
+          <a href="">Cadastre-se</a>
+          <a href="">Como funciona</a>
+          <a href="">Quem somos</a>
         </div>
         <div className="text-4xl flex flex-col">
           <div className="text-lg font-bold">Social</div>
@@ -35,7 +35,7 @@ const Footer: React.FC = () => {
         | 
         <a className="ml-1">Termos de uso</a>
       </div>
-      <div>
+      <div className="w-5/6 text-center flex items-center justify-center">
         ©. MSE Cazappi - Delivery app | Desenvolvido por CATI Jr.
       </div>
     </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Children  } from 'react';
 import { FLEXROW, FLEXCOLUMN, TEXT, TEXTINT, IMAGE, REGISTER, SPACE } from './style';
 import padariaImage from '../../assets/padariaImage.png';
 import verdurasImage from '../../assets/verdurasImage.png';
@@ -10,10 +10,39 @@ import Carousel from '../../components/Carousel/Carousel';
 import {responsiveWidth as rw,
   responsiveHeight as rh,
   responsiveFontSize as rf } from '../../utils/responsive-functions';
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Home = () => {
   const [selectedOption, setSelectedOption] = useState(0);
   const videoURL = "https://www.youtube.com/embed/yLgbyeFHd6k";
+  
+  const boxVariant = {
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, scale: 0 },
+  }
+
+  type Props = {
+    className: string;
+    children?: React.ReactNode;
+  };
+
+  const Animation = ({children, className}: Props) => {
+    const control = useAnimation()
+    const [ref, inView] = useInView()
+
+    useEffect(() => {
+      if (inView) {
+        control.start("visible");
+      } 
+    }, [control, inView]);
+
+    return (
+      <motion.div className={className} ref={ref} variants={boxVariant} initial="hidden" animate={control}>
+        {children}
+      </motion.div>
+    )
+  }
 
   const handleOptionChange = (option: number) => {
     setSelectedOption(option);
@@ -31,55 +60,76 @@ const Home = () => {
       {/* ------- Second section ------- */}
       <section className="flex flex-col items-center justify-center py-5">
 
-        <div className="text-3xl my-10">Aqui você encontra!</div>
+        <Animation className="text-3xl my-10">Aqui você encontra!</Animation>
 
-        <div className="italic text-center w-3/5 mb-10">
+        <Animation className="italic text-center w-3/5 mb-10">
           O Cazappi é uma empresa brasileira de tecnologia que oferece soluções de comércio de alimentos
           para que pessoas e empresas possam comprar, vender, pagar, anunciar e entregar produtos por meio
           da internet.
-        </div>
+        </Animation>
 
         <div className="flex flex-col md:flex-row items-center justify-between text-center md:my-10">
-          <div className="flex flex-col items-center justify-center my-10 md:my-0 w-2/3 md:w-1/5">
+          <Animation className="flex flex-col items-center justify-center my-10 md:my-0 w-2/3 md:w-1/5">
             <div className="h-36">
               <img src={padariaImage} alt="pães" />
             </div>
             <div>Uma padaria completa com doces, pães e bolos.</div>
-          </div>
-          <div className="flex flex-col items-center justify-center my-10 md:my-0 w-2/3 md:w-1/5">
+          </Animation>
+          <Animation className="flex flex-col items-center justify-center my-10 md:my-0 w-2/3 md:w-1/5">
             <div className="h-36">
               <img src={verdurasImage} alt="verduras" />
             </div>
             <div>Todos os tipos de verduras, legumes e frutas.</div>
-          </div>
-          <div className="flex flex-col items-center justify-center my-10 md:my-0 w-2/3 md:w-1/5">
+          </Animation>
+          <Animation className="flex flex-col items-center justify-center my-10 md:my-0 w-2/3 md:w-1/5">
             <div className="h-36">
               <img src={pizzaImage} alt="pizza" />
             </div>
             <div>Variedade de lanches, pizzas e porções.</div>
-          </div>
-          <div className="flex flex-col items-center justify-center my-10 md:my-0 w-2/3 md:w-1/5">
+          </Animation>
+          <Animation className="flex flex-col items-center justify-center my-10 md:my-0 w-2/3 md:w-1/5">
             <div className="h-36">
               <img src={mercadoImage} alt="mercado" />
             </div>
             <div>Um mercado completo ao seu lado.</div>
-          </div>
+          </Animation>
         </div>
 
       </section>
 
       {/* ------- Video section ------- */}
 
-      <section className="flex flex-col items-center justify-center py-5">
-        <div className="text-3xl my-10">Quem somos</div>
-        <div  className="bg-SECONDARY w-full h-full flex flex-row items-center justify-center">
-          <div className="w-1/2 h-full flex items-center justify-center text-WHITE text-xl md:text-4xl text-center">Assista ao VÍDEO e entenda melhor!</div>
-          <div className='videoWrapper w-1/2 rounded-ss-3xl'>
-            <iframe className="rounded-ss-3xl w-full h-96" src={videoURL} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-          </div>
-        </div>
+      <section className="flex flex-col items-center justify-center pt-5">
+        <Animation className="text-3xl my-10">Quem somos</Animation>
+        <Animation  className="bg-SECONDARY w-full h-full flex flex-col md:flex-row items-center justify-center">
+          <Animation className="w-5/6 h-24 md:w-1/2 md:h-full flex items-center justify-center text-WHITE text-xl md:text-4xl text-center">Assista ao VÍDEO e entenda melhor!</Animation>
+          <Animation className='videoWrapper w-full md:w-1/2 rounded-ss-3xl'>
+            <iframe className="rounded-ss-3xl w-full h-96 border-none" src={videoURL} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+          </Animation>
+        </Animation>
       </section>
 
+      {/* ------- Third section ------- */}
+
+      <section className="text-center flex flex-col items-center justify-center w-full text-xl md:text-3xl">
+        <Animation className="flex flex-col md:flex-row w-full">
+          <Animation className="border-GRAY_600 border-2 w-full md:w-1/3 h-64 px-8 flex items-center justify-center">Quanto mais você vender, mais você ganha!</Animation>
+          <Animation className="border-GRAY_600 border-2 w-full md:w-1/3 h-64 px-8 flex items-center justify-center">Plataforma de pagamento 100% segura.</Animation>
+          <Animation className="border-GRAY_600 border-2 w-full md:w-1/3 h-64 px-8 flex items-center justify-center">Venda para toda sua região</Animation>
+        </Animation>
+        <Animation className="flex flex-col md:flex-row w-full">
+          <Animation className="border-GRAY_600 border-2 w-full md:w-1/2 h-64 px-8 flex items-center justify-center">De forma simples e rápida cadastre seus produtos e acompanhe todo histórico de vendas, lucros, avaliações dos clientes.</Animation>
+          <Animation className="border-GRAY_600 border-2 w-full md:w-1/2 h-64 px-8 flex items-center justify-center">Chegou a hora de faturar com o seu pão, doces caseiros, lanches e até mesmo montando uma mercearia em sua casa.</Animation>
+        </Animation>
+      </section>
+
+      {/* ------- fourth section ------- */}
+
+      <Animation className="w-full h-48 flex items-center justify-center">
+        <button className="p-4 rounded-3xl bg-YELLOW text-xl md:text-3xl">
+          Indique para um amigo!
+        </button>
+      </Animation>
           
       {/* ----------------------- FOOTER ----------------------- */}    
       <Footer />
