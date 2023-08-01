bccad66d74e3c0c997558f98ac1e22d95f9623c5
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2023-08-01 17:45:31 -0300

    Feat: começando a tela register shop

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -6,12 +6,29 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
-    const [showPassword, setShowPassword] = React.useState(true);
+    const [showPassword, setShowPassword] = React.useState(false);
 
     const togglePasswordVisibility = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         setShowPassword(!showPassword);
     };
+
+    const icon = (className?.indexOf("iconOn") !== -1);
+    let iconName = "";
+    switch (type) {
+      case "file":
+        iconName = "material-symbols:drive-folder-upload-outline";
+        break;
+      case "text":
+        iconName = "material-symbols:mail-outline";
+        break;
+      case "number":
+        iconName = "mdi:numbers";
+        break;
+      default:
+        iconName = "";
+        break;
+    }
 
     const inputType = showPassword ? "text" : type;
 
@@ -20,12 +37,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={inputType}
           className={
-            "flex h-10 w-full rounded-xl outline-none border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            `flex h-10 w-full rounded-xl outline-none border-none bg-GRAY_300 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-none file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50
+            ${icon ? "pl-12" : ""}
+            `
           }
           ref={ref}
           {...props}
-          
         />
+
         {type === "password" && (
             <button className="absolute top-[55%] -translate-y-1/2 right-3 border-none bg-transparent cursor-pointer" onClick={togglePasswordVisibility}>
             {showPassword ? 
@@ -36,6 +55,16 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             }
             </button>
         )}
+
+        {icon ?
+          <div className="absolute top-[58%] -translate-y-1/2 left-3 border-none bg-transparent">
+            <Icon width={"22px"} icon={iconName}
+            className="text-GRAY_600">
+            </Icon>
+          </div>
+        :
+        ""
+        }
     </div>
     )
   }
--- src/pages/RegisterShop/index.tsx
+++ src/pages/RegisterShop/index.tsx
@@ -14,6 +14,8 @@ import profileExample from '../../assets/profileExample.png'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { url } from 'inspector';
+import Input from '../../components/Input/Input';
+
 
 const RegisterShop = () => {
     const navigate = useNavigate();
@@ -28,11 +30,57 @@ const RegisterShop = () => {
 
             {/* ----------------------- Container ----------------------- */}
             <div className='w-full flex flex-col items-center justify-center my-14'>
-                <div className='text-4xl w-4/5 text-center text-PRIMARY text-center font-bold'>Cadastro de loja</div>
-                <div className='my-6 w-4/5 bg-PRIMARY h-[2px]'></div>
-                <div className='text-lg mb-6 w-4/5 text-black font-medium text-center'>Cadastre-se e desfrute do nosso sistema de vendas!</div>
+                <div className='text-4xl w-3/5 text-center text-PRIMARY text-center font-bold'>Cadastro de loja</div>
+                <div className='my-6 w-3/5 bg-PRIMARY h-[2px]'></div>
+                <div className='text-lg mb-6 w-3/5 text-black font-medium text-center'>Cadastre-se e desfrute do nosso sistema de vendas!</div>
 
+                <div className='w-3/5'>
+                    <div className='w-1/2 mb-6'>
+                        <label className='text-base mb-5' htmlFor="capa-input">Selecionar imagem de capa</label>
+                        <Input className='iconOn' type='file' id="capa-input"></Input>
+                    </div>
+                    <div className='w-1/2 mb-6'>
+                        <label className='text-base mb-5' htmlFor="profile-input">Selecionar imagem de perfil</label>
+                        <Input className='iconOn' type='file' id="profile-input"></Input>
+                    </div>
 
+                    <div className='w-full mb-6'>
+                        <label htmlFor="">Informe a localização do seu negócio</label>
+
+                        <div className='mb-2'>
+                            <Input className='iconOn' type='text' placeholder='CEP'></Input>
+                        </div>
+
+                        <div className='flex w-full flex-col md:flex-row justify-between mb-2'>
+                            <div className='md:w-3/12'>
+                                <Input className='iconOn' type='text' placeholder='Estado'></Input>
+                            </div>
+                            <div className='md:w-8/12'>
+                                <Input className='iconOn'   type='text' placeholder='Cidade'></Input>
+                            </div>
+                        </div>
+
+                        <div className='mb-2'>
+                            <Input className='iconOn' type='text' placeholder='Bairro'></Input>
+                        </div>
+
+                        <div className='flex w-full flex-col md:flex-row justify-between mb-2'>
+                            <div className='md:w-8/12'>
+                                <Input className='iconOn' type='text' placeholder='Rua'></Input>
+                            </div>
+                            <div className='md:w-3/12'>
+                                <Input className='iconOn'   type='number' placeholder='Número'></Input>
+                            </div>
+                        </div>
+
+                        <div className=''>
+                            <Input className='iconOn' type='text' placeholder='Complemento'></Input>
+                        </div>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
             </div>
 
             {/* ----------------------- FOOTER ----------------------- */}
