4c1029df0ffed2da20070ee4826c791b267ce4b0
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2023-08-07 13:36:08 -0300

    Fix: corrigindo campo de input

--- src/components/Input/InputFile.tsx
+++ src/components/Input/InputFile.tsx
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Icon } from '@iconify-icon/react';
+import { input } from '@material-tailwind/react';
 
 interface FileInputProps {
     className?: string;
@@ -7,10 +8,11 @@ interface FileInputProps {
 
 const FileInput: React.FC<FileInputProps> = ({ className }) => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [fileName, setFileName] = useState("Escolher arquivo");
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // Se você quiser realizar alguma ação quando um arquivo for selecionado, você pode fazer aqui.
-    // A referência para o input é `inputRef.current`.
+    const files = inputRef.current?.files;
+    inputRef.current ? setFileName(files?.item(0)?.name+"") : setFileName("Escolher arquivo");
   };
 
   const icon = (className?.indexOf("iconOn") !== -1 && className?.indexOf("iconOn") !== undefined);
@@ -19,11 +21,12 @@ const FileInput: React.FC<FileInputProps> = ({ className }) => {
     <label className={`relative flex items-center w-full h-10 rounded-xl bg-GRAY_300 px-3 py-2 text-sm cursor-pointer ${className}`}>
       <input
         type="file"
+        // accept='pdf/*' // Analisar e automatizar no component
         className={`absolute w-full h-full opacity-0 cursor-pointer ${icon ? "pl-12" : ""}`}
         ref={inputRef}
         onChange={handleFileChange}
       />
-      <span className={`${icon ? "ml-8" : ""} text-GRAY_600`}>Escolher arquivo</span>
+      <span className={`${icon ? "ml-8" : ""} text-GRAY_600`}>{fileName}</span>
       
         {icon ?
             <div className="absolute top-[58%] -translate-y-1/2 left-3 border-none bg-transparent">
