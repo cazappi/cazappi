0f8679ad55e157396f71a907dc6054249afa96b8
yasminvo <yasminvo@estudante.ufscar.br>
2024-04-29 16:38:50 -0300

    feat/bag-list

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -20,7 +20,6 @@ import Unauthenticated from "../pages/Unauthenticated";
 import PrivateRoute from "./private-route";
 import HasShopVerificationRoute from "./ShopAndLog-route";
 import UpdateShop from "../pages/UpdateShop";
-import BagList from "../pages/BagList"
 
 const AppRoutes = () => (
   <>
@@ -39,7 +38,6 @@ const AppRoutes = () => (
       <Route path="/BusinessType" element={<BusinessType />} />
       <Route path="/ShopConditions" element={<ShopConditions />} />
       <Route path="/unauthorized" element={<Unauthenticated />} />
-      <Route path="/BagList" element={<BagList />} />
     </Routes>
     <HasShopVerificationRoute path="/registershop" element={<RegisterShop />} />
     <PrivateRoute path="/profile" element={<Profile />} />
